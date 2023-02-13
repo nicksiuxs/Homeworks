ec1e4848432118d065b1aa0d2f1c8fa38896ad8e
Nicolás Londoño <nicolas.londono@veevart.com>
2023-02-12 20:39:43 -0500

    Add: final methods arrays

--- index.js
+++ index.js
@@ -252,3 +252,129 @@ console.log("method pop:", evenNumbers.pop());
 */
 evenNumbers.push(13);
 console.log("method push:", evenNumbers);
+
+// ----------------------
+
+/**
+ *  Method Array.prototype.reduce()
+ *  method executes a user-supplied "reducer" callback function on each element of the array, in order, passing in the return value 
+ *  from the calculation on the preceding element. The final result of running the reducer across all elements of the array is 
+ *  a single value.
+ * 
+ *  The first time that the callback is run there is no "return value of the previous calculation". If supplied, an initial value may 
+ *  be used in its place. Otherwise the array element at index 0 is used as the initial value and iteration starts from the next element 
+ *  (index 1 instead of index 0).
+ * 
+ *  Perhaps the easiest-to-understand case for reduce() is to return the sum of all the elements in an array:
+*/
+const sumEvenNumbers = evenNumbers.reduce((accum, currenValue) => {
+    return accum + currenValue
+}, 0);
+console.log("method reduce:", sumEvenNumbers);
+
+// ----------------------
+
+/** 
+ *  Method Array.prototype.reduceRight()
+ *  method applies a function against an accumulator and each value of the array (from right-to-left) to reduce it to a single value.
+*/
+const arrayOfArrays = [[1, 2], [3, 4], [5, 6]]
+const newArray = arrayOfArrays.reduceRight((accum, currenValue) => accum.concat(currenValue));
+console.log("method reduceRight:", newArray);
+
+// ----------------------
+
+/** 
+ *  Method Array.prototype.reverse()
+ *  method reverses an array in place and returns the reference to the same array, the first array element now becoming the last, 
+ *  and the last array element becoming the first. In other words, elements order in the array will be turned towards the direction 
+ *  opposite to that previously stated.
+*/
+console.log("method reverse:", newArray.reverse());
+
+// ----------------------
+
+/**
+ *  Method Array.prototype.shift()
+ *  method removes the first element from an array and returns that removed element. This method changes the length of the array.
+*/
+console.log("method shift:", newArray.shift());
+
+// ----------------------
+
+/**
+ *  Method Array.prototype.slice()
+ *  method returns a shallow copy of a portion of an array into a new array object selected from start to end (end not included) 
+ *  where start and end represent the index of items in that array. The original array will not be modified.
+*/
+console.log('method slice:', newArray.slice(2, 4));
+
+// ----------------------
+
+/**
+ *  Method Array.prototype.some()
+ *  method tests whether at least one element in the array passes the test implemented by the provided function. It returns true if,
+ *  in the array, it finds an element for which the provided function returns true; otherwise it returns false. It doesn't modify the array.
+*/
+console.log('method some:', oddNumbers.some(number => number % 2 === 1));
+
+// ----------------------
+
+/**
+ *  Method Array.prototype.sort()
+ *  method sorts the elements of an array in place and returns the reference to the same array, now sorted. The default sort order is ascending,
+ *  built upon converting the elements into strings, then comparing their sequences of UTF-16 code units values.
+*/
+const mixedNumbers = oddNumbers.concat(evenNumbers);
+console.log("method sort:", mixedNumbers.sort((a, b) => a - b));
+
+// ----------------------
+
+/**
+ *  Method Array.prototype.splice()
+ *  method changes the contents of an array by removing or replacing existing elements and/or adding new elements in place. To access part of an 
+ *  array without modifying it, see slice().
+*/
+console.log("method splice before:", newArray);
+newArray.splice(1, 0, 2)
+console.log("method splice after:", newArray);
+
+// ----------------------
+
+/**
+ *  Method Array.prototype.toLocaleString()
+ *  method returns a string representing the elements of the array. The elements are converted to Strings using their toLocaleString methods and 
+ *  these Strings are separated by a locale-specific String (such as a comma ",").
+*/
+const mixed = [1, 2, 3, 4];
+console.log("method toLocaleString:", mixed.toLocaleString());
+
+// ----------------------
+
+/**
+ *  Method Array.prototype.toString()
+ *  method returns a string representing the specified array and its elements.
+*/
+console.log("method toString:", mixed.toString());
+
+// ----------------------
+
+/**
+ *  Method Array.prototype.unshift()
+ *  method adds one or more elements to the beginning of an array and returns the new length of the array.
+*/
+mixed.unshift(-2, -1, 0);
+console.log("method unshift:", mixed);
+
+// ----------------------
+
+/**
+ *  Method Array.prototype.values()
+ *  method returns a new array iterator object that iterates the value of each item in the array.
+*/
+const iterator2 = mixed.values();
+console.log("method values start:");
+for (const value of iterator2) {
+    console.log(value)
+}
+console.log("method values end:");
